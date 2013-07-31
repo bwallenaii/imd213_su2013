b909a4002781f3cf5a4f3ba7b08be0041725a555
Brent Allen <bwallen.aii@gmail.com>
2013-07-31 03:26:59 +0000

    week 4 added

---
+++ week_4/ajaxprocess.php
@@ -0,0 +1,22 @@
+<?php
+$data = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+$message = "";
+
+foreach($data as $name => $value )
+{
+	if(is_array($value))
+	{
+		$message .= "$name: ".implode(", ", $value);
+	}
+	else
+	{
+		$message .= "$name: $value\r\n";
+	}
+}
+
+mail("bwallen.aii@gmail.com", "Lottery Entry", $message);
+
+?>
+	<h1>Thank You!!</h1>
+	<p>We appreciate your entry. We will contact you if you win. We will not post the winner to 
+	protect them. And us. Mostly us. So, we probably won't be contacting you. Thanks!</p>
